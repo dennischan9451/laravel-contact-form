e8c1b50e0cf3f5fbce872e67888241070c9fb690
Matias Diaz <matiasdev51@gmail.com>
2020-12-15 19:55:42 +0700

    create a simple unit test for get request

--- tests/Feature/ContactTest.php
+++ tests/Feature/ContactTest.php
@@ -3,18 +3,19 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class ContactTest extends TestCase
 {
     /**
-     * A basic test example.
+     * A basic feature test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testExample()
     {
-        $response = $this->get('/');
+        $response = $this->get('/contact');
 
         $response->assertStatus(200);
     }
