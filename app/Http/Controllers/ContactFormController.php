cb15fb82f81066bb6c5b2ba0f88e35654cee06d2
Matias Diaz <matiasdev51@gmail.com>
2020-12-15 19:12:14 +0700

    create migration, model, route & controller

---
+++ app/Http/Controllers/ContactFormController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+
+class ContactFormController extends Controller
+{
+    // Contact Form
+    public function index(Request $request) {
+        return view('contact');
+    }
+
+    // Store Contact Form data
+    public function storeFormData(Request $request) {
+
+        // Form validation
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'message' => 'required'
+        ]);
+
+        //  Store data in database
+        Contact::create($request->all());
+
+        return back()->with('success', 'Thank you for writing to us.');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactFormController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', [ContactFormController::class, 'index']);
+
+Route::post('/contact', [ContactFormController::class, 'storeFormData'])->name('contact.store');
+
